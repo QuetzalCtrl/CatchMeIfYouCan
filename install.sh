bc224408b2c4ed8a5b183cc5ca828f7342034427
QuetzalCtrl <hugo.vanrobaeys@gmail.com>
2022-12-28 19:31:28 +0700

    modifs mineures

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# copy the quetzal script to the /usr/bin/ directory
+sudo cp ifucan /usr/bin/
+# make the quetzal script executable
+sudo chmod +x /usr/bin/ifucan
+
+echo "Installation complete"
